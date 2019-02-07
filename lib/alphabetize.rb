a8d3dae32ebb750fc758f782b06f6bd398c36bd3
Rufus <ruufman@gmail.com>
2019-02-07 22:01:45 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
 def alphabetize(arr)
-  # code here
+  var x = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+  for(int i = 0; i < x.length; i++)
+    {
+    console.log(x[i]);
+    }
+    array.sort_by(x);
+    console.log("After sort :");
+    for(int i = 0; i < x.length; i++)
+      {
+      console.log(x[i]);
+      }
 end
